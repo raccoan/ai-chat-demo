4ce187008bd8a01fedeba2b59385a37c658fd305
lq <19934208847@163.com>
2026-06-08 21:12:33 +0800

    修改了一下样式

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -1,5 +1,9 @@
 import ReactMarkdown from 'react-markdown'
 
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
 interface MessageItemProps {
   role: 'user' | 'assistant'
   content: string
@@ -14,7 +18,28 @@ function MessageItem(props: MessageItemProps) {
           : 'message assistant'
       }
     >
-      <ReactMarkdown>{props.content}</ReactMarkdown>
+      <ReactMarkdown
+        components={{
+          code({ className, children }) {
+            const match = /language-(\w+)/.exec(
+              className || ''
+            )
+
+            return match ? (
+              <SyntaxHighlighter
+                style={oneDark}
+                language={match[1]}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code>{children}</code>
+            )
+          }
+        }}
+      >
+        {props.content}
+      </ReactMarkdown>
     </div>
   )
 }
